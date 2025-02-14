375c00f7c1bd0c0cf89dee130ed97bd40c36a37f
Leonardo Gonçalves Cordeiro Cruz Filho <leonardogccf@gmail.com>
2025-02-14 17:19:42 -0300

    Class Curso e Mentoria

---
+++ src/codigos/Main.java
@@ -0,0 +1,36 @@
+package codigos;
+
+import java.time.LocalDate;
+
+import br.com.dio.desafio.dominio.Curso;
+import br.com.dio.desafio.dominio.Mentoria;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Curso curso1 = new Curso();
+		
+		curso1.setTitulo(" Curso Java ");
+		curso1.setDescricao(" descrção curso java ");
+		curso1.setCargaHoraria(8);
+		
+		Curso curso2 = new Curso();
+		curso2.setTitulo(" Curso JavaScript ");
+		curso2.setDescricao(" descrção curso js ");
+		curso2.setCargaHoraria(4);
+		
+		Mentoria mentoria = new Mentoria();
+		mentoria.setTitulo(" Mentoria Java ");
+		mentoria.setDescricao(" Descvrição Mentoria Java ");
+		mentoria.setData(LocalDate.now());
+		
+		
+		System.out.println(curso1);
+		System.out.println(curso2);
+		
+
+	}
+
+}
